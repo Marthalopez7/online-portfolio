32f1ba1a2015428842220fe8802ea73c4dc38aad
Marthalopez7 <116187271+Marthalopez7@users.noreply.github.com>
2023-05-08 10:35:19 -0700

    Css and js update
    Tried to do the image carousel, but it did not work. Updated so all pages are done styling without animations and js. Wrote to do list

--- app.js
+++ app.js
@@ -13,3 +13,27 @@ button.onclick = function () {
     overlay.style.display = 'block';
     popUpOne.style.display = 'block';
 }
+
+var i = 0
+var images = [];
+var time = 5000;
+
+images[0] = 'images/a-wish-from-the-earth-icon.jpg'
+images[1] = 'images/battlewitch-icon.jpg'
+images[2] = 'images/delvehomes-icon.jpg'
+images[3] = 'images/pursuit-of-healthyness-poster-icon.jpg'
+images[4] = 'images/gov-video.jpg'
+images[5] = 'images/Career-research-essay.jpg'
+images[6] = 'images/reflective-essay.jpg'
+
+function slideshow() {
+    document.slide.src = images[i];
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0
+    }
+    setTimeout('sildeshow()', time)
+
+}
+window.onload = slideshow
