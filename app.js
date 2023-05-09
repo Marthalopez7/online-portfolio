64008e06ff8d11f1a873993ae3cef43990c6121a
Marthalopez7 <116187271+Marthalopez7@users.noreply.github.com>
2023-05-09 10:32:32 -0700

    JS update
    Updated the popcontent

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const overlay = document.getElementById('overlay')
-const popUpOne = document.getElementById('popup')
-const closeButton = document.getElementById('popupclose')
-const button = document.getElementById('button')
+// const overlay = document.getElementById('overlay')
+// const popUpOne = document.getElementById('popup')
+// const closeButton = document.getElementById('popupclose')
+// const button = document.getElementById('button')
 
 
-closeButton.onclick = function() {
-    overlay.style.display = 'none';
-    popUpOne.style.display = 'none';
-}
+// closeButton.onclick = function() {
+//     overlay.style.display = 'none';
+//     popUpOne.style.display = 'none';
+// }
 
-button.onclick = function () {
-    overlay.style.display = 'block';
-    popUpOne.style.display = 'block';
-}
+// button.onclick = function () {
+//     overlay.style.display = 'block';
+//     popUpOne.style.display = 'block';
+// }
 
-var i = 0
-var images = [];
-var time = 5000;
+// var i = 0
+// var images = [];
+// var time = 5000;
 
-images[0] = 'images/a-wish-from-the-earth-icon.jpg'
-images[1] = 'images/battlewitch-icon.jpg'
-images[2] = 'images/delvehomes-icon.jpg'
-images[3] = 'images/pursuit-of-healthyness-poster-icon.jpg'
-images[4] = 'images/gov-video.jpg'
-images[5] = 'images/Career-research-essay.jpg'
-images[6] = 'images/reflective-essay.jpg'
+// images[0] = 'images/a-wish-from-the-earth-icon.jpg'
+// images[1] = 'images/battlewitch-icon.jpg'
+// images[2] = 'images/delvehomes-icon.jpg'
+// images[3] = 'images/pursuit-of-healthyness-poster-icon.jpg'
+// images[4] = 'images/gov-video.jpg'
+// images[5] = 'images/Career-research-essay.jpg'
+// images[6] = 'images/reflective-essay.jpg'
 
-function slideshow() {
-    document.slide.src = images[i];
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0
-    }
-    setTimeout('sildeshow()', time)
+// function slideshow() {
+//     document.slide.src = images[i];
+//     if (i < images.length - 1) {
+//         i++;
+//     } else {
+//         i = 0
+//     }
+//     setTimeout('sildeshow()', time)
 
-}
-window.onload = slideshow
+// }
+// window.onload = slideshow
