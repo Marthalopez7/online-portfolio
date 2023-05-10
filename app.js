83ca34adcf2fc46fd6223dac7e5b40d7a43ffdca
Marthalopez7 <116187271+Marthalopez7@users.noreply.github.com>
2023-05-10 10:41:06 -0700

    Slideshow Complete
    Started the pop up yet it failed, have to fix it at home. Then I finished the slideshow on the home page. Although, I have to cut some of the pictures because I also have to create a carousel

--- app.js
+++ app.js
@@ -14,27 +14,36 @@
 //     popUpOne.style.display = 'block';
 // }
 
-// var i = 0
-// var images = [];
-// var time = 5000;
+var i = 0
+var images = [];
+var time = 5000;
 
-// images[0] = 'images/a-wish-from-the-earth-icon.jpg'
-// images[1] = 'images/battlewitch-icon.jpg'
-// images[2] = 'images/delvehomes-icon.jpg'
-// images[3] = 'images/pursuit-of-healthyness-poster-icon.jpg'
-// images[4] = 'images/gov-video.jpg'
-// images[5] = 'images/Career-research-essay.jpg'
-// images[6] = 'images/reflective-essay.jpg'
+images[0] = 'images/a-wish-from-the-earth-icon.jpg'
+images[1] = 'images/battlewitch-icon.jpg'
+images[2] = 'images/delvehomes-icon.jpg'
+images[3] = 'images/pursuit-of-healthyness-poster-icon.jpg'
+images[4] = 'images/gov-video.jpg'
+images[5] = 'images/Career-research-essay.jpg'
+images[6] = 'images/reflective-essay.jpg'
 
-// function slideshow() {
-//     document.slide.src = images[i];
-//     if (i < images.length - 1) {
-//         i++;
-//     } else {
-//         i = 0
-//     }
-//     setTimeout('sildeshow()', time)
+function slideshow() {
+    document.slide.src = images[i];
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0
+    }
+    setTimeout('slideshow()', time)
 
-// }
-// window.onload = slideshow
+}
+window.onload = slideshow
 
+// var imgbuttons = document.querySelectorAll('#button');
+
+// imgbuttons.forEach(function(btn) {
+//     btn.onclick = function() {
+//         var modal = btn.getAttribute('data-modal');
+//         document.getElementById('modal1').style.display = "block";
+        
+//     };
+// });
